ea48b540ef1b08e3e5248ff4689b6f4f95180d4a
Donate_ro <joke4789@ya.ru>
2019-05-29 17:26:44 +0300

    DataSource is replaced from the database file to the dynamical DataTable object

--- CS/WindowsApplication1/Form1.cs
+++ CS/WindowsApplication1/Form1.cs
@@ -1,25 +1,41 @@
 using System;
-using System.Collections.Generic;
-using System.ComponentModel;
 using System.Data;
-using System.Drawing;
-using System.Text;
 using System.Windows.Forms;
 
-namespace WindowsApplication1 {
-    public partial class Form1: Form {
-        public Form1() {
+namespace WindowsApplication1
+{
+    public partial class Form1 : Form
+    {
+        Random random = new Random();
+
+        public Form1()
+        {
             InitializeComponent();
         }
 
-        private void button1_Click(object sender,EventArgs e) {
+        private void button1_Click(object sender, EventArgs e)
+        {
             gridControl1.ShowPrintPreview();
         }
 
-        private void Form1_Load(object sender,EventArgs e) {
-            // TODO: This line of code loads data into the 'nwindDataSet.Products' table. You can move, or remove it, as needed.
-            this.productsTableAdapter.Fill(this.nwindDataSet.Products);
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            productsBindingSource.DataSource = GetProductsDataTable();
+        }
 
+        DataTable GetProductsDataTable()
+        {
+            DataTable table = new DataTable();
+            table.TableName = "Products";
+            table.Columns.Add(new DataColumn("ProductName", typeof(string)));
+            table.Columns.Add(new DataColumn("UnitPrice", typeof(double)));
+            table.Columns.Add(new DataColumn("UnitsOnOrder", typeof(int)));
+            for(int i = 0; i < 20; i++)
+            {
+                int index = i + 1;
+                table.Rows.Add("Product " + index, Math.Round(random.NextDouble() * 1000, 2), random.Next(0, 9) * 10);
+            }
+            return table;
         }
     }
 }
--- VB/WindowsApplication1/Form1.vb
+++ VB/WindowsApplication1/Form1.vb
@@ -1,27 +1,38 @@
-Imports Microsoft.VisualBasic
 Imports System
-Imports System.Collections.Generic
-Imports System.ComponentModel
 Imports System.Data
-Imports System.Drawing
-Imports System.Text
 Imports System.Windows.Forms
 
 Namespace WindowsApplication1
-	Partial Public Class Form1
-		Inherits Form
-		Public Sub New()
-			InitializeComponent()
-		End Sub
+    Partial Public Class Form1
+        Inherits Form
 
-		Private Sub button1_Click(ByVal sender As Object, ByVal e As EventArgs) Handles button1.Click
-			gridControl1.ShowPrintPreview()
-		End Sub
+        Private random As Random = New Random()
 
-		Private Sub Form1_Load(ByVal sender As Object, ByVal e As EventArgs) Handles MyBase.Load
-			' TODO: This line of code loads data into the 'nwindDataSet.Products' table. You can move, or remove it, as needed.
-			Me.productsTableAdapter.Fill(Me.nwindDataSet.Products)
+        Public Sub New()
+            InitializeComponent()
+        End Sub
 
-		End Sub
-	End Class
+        Private Sub button1_Click(ByVal sender As Object, ByVal e As EventArgs)
+            gridControl1.ShowPrintPreview()
+        End Sub
+
+        Private Sub Form1_Load(ByVal sender As Object, ByVal e As EventArgs)
+            productsBindingSource.DataSource = GetProductsDataTable()
+        End Sub
+
+        Private Function GetProductsDataTable() As DataTable
+            Dim table As DataTable = New DataTable()
+            table.TableName = "Products"
+            table.Columns.Add(New DataColumn("ProductName", GetType(String)))
+            table.Columns.Add(New DataColumn("UnitPrice", GetType(Double)))
+            table.Columns.Add(New DataColumn("UnitsOnOrder", GetType(Integer)))
+
+            For i As Integer = 0 To 20 - 1
+                Dim index As Integer = i + 1
+                table.Rows.Add("Product " & index, Math.Round(random.NextDouble() * 1000, 2), random.[Next](0, 9) * 10)
+            Next
+
+            Return table
+        End Function
+    End Class
 End Namespace
